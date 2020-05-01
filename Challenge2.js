5f2940629368ed2bb3a0886af6187bb2812a5f94
Akhil Joshi <ashwini.singh@externals.hilti.com>
2020-05-02 02:49:57 +0530

    Challenge 2

---
+++ Challenge2.js
@@ -0,0 +1,6 @@
+scre1 = 12 + 12 + 15;
+scre2 = 12 + 19 + 17;
+
+if (scr1 > scr2) {
+  console.log("team1 Wins");
+} else console.log("team2 Wins");
---
+++ quickDemoOnTernary.js
@@ -0,0 +1,17 @@
+// Ternary is use to avoid one if else
+
+days = 21;
+days > 20 ? console.log("Few Days left") : console.log("Many Days are left");
+
+// Switch
+
+switch (days) {
+  case 21:
+    console.log("Few Days Are left");
+    break;
+  case 10:
+    console.log("Many Days Are there");
+    break;
+  default:
+    console.log("Enter the days");
+}
